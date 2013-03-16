a5d375cf24c04510df4afc989a3bec97ee5a23bc
Jason Ray <jasonray@jayray.net>
2013-03-16 16:21:53 -0400

    cleanup formatting

--- src/main/java/course/BlogPostDAO.java
+++ src/main/java/course/BlogPostDAO.java
@@ -91,13 +91,6 @@ public class BlogPostDAO {
     // Append a comment to a blog post
     public void addPostComment(final String name, final String email, final String body,
                                final String permalink) {
-
-        // XXX HW 3.3, Work Here
-        // Hints:
-        // - email is optional and may come in NULL. Check for that.
-        // - best solution uses an update command to the database and a suitable
-        //   operator to append the comment on to any existing list of comments
-
         BasicDBObject filter = new BasicDBObject("permalink", permalink);
 
         BasicDBObject commentRecord = new BasicDBObject("body", body);
